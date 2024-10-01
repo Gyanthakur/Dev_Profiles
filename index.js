3fbcc7f2460d07105826289b17a4690b4ac28722
Gyanthakur <gps.96169@gmail.com>
2024-10-01 20:43:00 +0530

    added nodejs

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ async function run() {
     const issueMessage = core.getInput('issue-message');
     const prMessage = core.getInput('pr-message');
     const footer = core.getInput('footer') || '';
-    const assignUser = core.getInput('assign-user');  // This gets the user to assign (can be specific or author)
 
     const octokit = github.getOctokit(githubToken);
     const context = github.context;
@@ -21,13 +20,6 @@ async function run() {
         issue_number: context.payload.issue.number,
         body: issueComment,
       });
-
-      // Auto-assign user to issue
-      await octokit.issues.addAssignees({
-        ...context.repo,
-        issue_number: context.payload.issue.number,
-        assignees: [assignUser],
-      });
     } else if (context.payload.pull_request) {
       // This is a pull request
       const prComment = `${prMessage}\n\n${footer}`;
@@ -35,13 +27,6 @@ async function run() {
         ...context.repo,
         issue_number: context.payload.pull_request.number,
         body: prComment,
-      });
-
-      // Auto-assign user to PR
-      await octokit.issues.addAssignees({
-        ...context.repo,
-        issue_number: context.payload.pull_request.number,
-        assignees: [assignUser],
       });
     }
   } catch (error) {
