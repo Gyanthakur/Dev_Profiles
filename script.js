96bba99ee74a3656198b68774dec255d9fa6ae66
Disha <smilesniyati@gmail.com>
2024-10-08 20:32:08 +0530

    added scroll to top button to all the pages

--- script.js
+++ script.js
@@ -46,3 +46,24 @@ darkMode.addEventListener("click", () => {
     });
 });
 });
+
+// Get the button
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks the button, scroll to the top of the document
+backToTopBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
